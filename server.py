4fedf88ad937f526b799345b0ffc17a9be8630b3
Walker60 <Chris.ak.walker@gmail.com>
2017-02-21 17:04:50 -0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify
+from flask import send_from_directory
+from os import listdir, path
+import json
+
+app = Flask(__name__)
+
+@app.route("/charts/", methods=['GET'])
+def getAllCharts():
+    try:
+        responseJson = []
+        workingDirPath = "/var/www/iChartsFlaskApp/iChartsFlaskApp/charts"
+        for file in listdir(workingDirPath):
+            modelDirectory = path.join(workingDirPath, file)
+            modelFile = path.join(modelDirectory, file + "model.json")
+            if path.isdir(modelDirectory) and path.isfile(modelFile):
+                model = open(modelFile, "r")
+                responseJson.append(json.load(model))
+        return jsonify(responseJson)
+    except Exception, e:
+        return(str(e))
+
+@app.route("/charts/<city>", methods=['GET'])
+def downloadmodel(city):
+
+    try:
+        city = city.upper()
+        with open("/home/icharts/icharts/charts/" + city + "/" + city + "model.json", "r") as json_data:
+            data = json.load(json_data)
+            return jsonify(data)
+
+    except Exception, e:
+        return(str(e))
+
+@app.route("/charts/<city>/zip", methods=['GET'])
+def downloadchart(city):
+
+    try:
+        city = city.upper()
+        with open("/home/icharts/icharts/charts/" + city + "/" + city + "model.json", "r") as json_data:
+            data = json.load(json_data)
+            version = data["version"]
+            return send_from_directory(directory="/home/icharts/icharts/charts/" + city + "/" + version, filename = city + ".zip")
+    except Exception, e:
+        return(str(e))
+
+
+if __name__  == "__main__":
+    app.run()
